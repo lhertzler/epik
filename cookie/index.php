2ca7cacfcca32a09d32187090e97e718fcf4a0b4
lhertzler <lukehertzler@gmail.com>
2018-06-19 20:15:59 -0700

    all

---
+++ cookie/index.php
@@ -0,0 +1,12 @@
+<?php
+
+
+startsession();
+
+$name = "Luke";
+$last = "Hertzler"
+
+echo $_COOKIE[$name] . $_COOKIE[$last];
+
+
+ ?>
--- wp/wp-config.php
+++ wp/wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'epik-db');
+define('DB_NAME', 'epik_wp');
 
 /** MySQL database username */
 define('DB_USER', 'root');
@@ -32,7 +32,7 @@ define('DB_PASSWORD', 'root');
 define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
+define('DB_CHARSET', 'utf8mb4');
 
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
@@ -46,14 +46,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
+define('AUTH_KEY',         'OcV?`%HQp)XKChcO*O#r#qnz4=%t78!a,.aJ=8t&/D@<aee a0D[SvE3nUR2&T;g');
+define('SECURE_AUTH_KEY',  'qpw#mpkVLCd{+<3%F|uG4yCDi#*|>sXuW|5H+NWn87j/DH5vzSA{cAaz7!m-8wJ=');
+define('LOGGED_IN_KEY',    ']|Tz2+ Sod}>}cKJXr*>;dUcyf&q0a>1AlLr@J:@Lq(UP9p?{lyCbYchp4#imx?V');
+define('NONCE_KEY',        'JLP7RX@9l=yd/JXzF31QAY?$V$$/BdrMi7/a`7lPNTT_;o15<d8|O.H}UQRC{A76');
+define('AUTH_SALT',        'ARF=fzEnD:I:F<_zRDv<KMCcR!HkQ>nL$~KBO.49.RbPMGZQM@S[#&~PeT4BT;#L');
+define('SECURE_AUTH_SALT', 'E1Dy{=)nmc bt3G^tn>m,!{I?_6_h{AmZ7xt:iyL2DT-CPp(T9IFefL}xr.ySIL#');
+define('LOGGED_IN_SALT',   'vW$b>K P?N(pGq2j1%g#Y#c`<-9ZV;i-3IL49gNi8mC[5p S0t@94}vP@7u+#F#F');
+define('NONCE_SALT',       'L4`tp[ FiN}egwZEF^W-;G::]H2`_Mgk@0Bu+tjjrp!Yzj +A`,u0[7^3,VgxI@J');
 
 /**#@-*/
 
